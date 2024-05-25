6698cf39a17c86048f0e13a7a42afe58fb3127f3
Szymek <Denek222@gmail.com>
2024-05-25 16:15:45 +0200

    [ADD] Deleting tickets and Link to ticket update

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,7 +1,7 @@
 import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
-export async function Delete(req, { params }) {
+export async function DELETE(req, { params }) {
     try {
         const { id } = params;
         await Ticket.findByIdAndDelete(id);
